85943dc0a8acd7af9262207ea97f149cef6ad936
michalinaj <michalina@jablonska.me>
2019-03-13 16:12:19 -0400

    question and answer model

---
+++ app/controllers/questions_controller.rb
@@ -0,0 +1,12 @@
+class QuestionsController < ApplicationController
+
+  def index
+    @questions = Question.all
+  end
+
+  def show
+    @question = Question.find(params[:id])
+    render :show
+  end
+
+end
