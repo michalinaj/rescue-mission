14f245e5a32d3f7fb517b7d37b861ad891a33279
michalinaj <michalina@jablonska.me>
2019-04-10 21:36:11 -0400

    delete option for questions

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -42,6 +42,12 @@ class QuestionsController < ApplicationController
     end
   end
 
+  def destroy
+    @question = Question.find(params[:id])
+    @question.destroy
+    redirect_to questions_path
+  end
+
   private
   def question_params
     params.require(:question).permit(:title, :description)
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,9 @@
 Rails.application.routes.draw do
   root 'questions#index'
   devise_for :users
-  resources :questions, only: [:index, :show, :new, :create, :edit, :update] do
+  resources :questions, only: [:index, :show, :new, :create, :edit, :update, :destroy] do
     resources :answers, only: [:index, :show, :new, :create]
   end
-  #
   # resources :answers, only: [:show]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
